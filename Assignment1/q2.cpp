3e37da4fed4766108245c372d7887c390a0c995d
Anwesha Bhowmick <113498742+Anwesha-Bhowmick@users.noreply.github.com>
2024-08-15 20:44:50 +0530

    Update q2.cpp

--- Assignment1/q2.cpp
+++ Assignment1/q2.cpp
@@ -7,6 +7,7 @@ int main()
   std::cin>>x;
   expression=(((x+1/x)+1/(x+1/x)) + 1/((x+1/x)+1/(x+1/x)));
   std::cout<<"\n the numerical value of the expression is : "<<expression<<"\n";
+  return 0;
 }
 
 
