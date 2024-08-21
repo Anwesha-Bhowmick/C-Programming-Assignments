ebb8de625df008ddda8f851463894c3a8d09474f
Anwesha Bhowmick <113498742+Anwesha-Bhowmick@users.noreply.github.com>
2024-08-21 16:00:25 +0530

    Update q5.cpp

--- Assignment1/q5.cpp
+++ Assignment1/q5.cpp
@@ -1,16 +1,16 @@
+
 #include<iostream>
 
 int main()
 {
-  double n1, k, n2;
-  int round;
+  long double n1, k, n2;
+  long int round;
   std::cout<<"\nenter a real number: ";
   std::cin>>n1;
   std::cout<<"\nenter the place till which the number is to be rounded off (k) : ";
   std::cin>>k;
-  round=int((n1*k) + 0.5);
-  n2=double(round/k);
+  round= (long int)((long int)(n1*k) + 0.5);
+  n2=(double)round/k;
   std::cout<<"\nthe rounded off number is : "<<n2<<"\n";
   return 0;
-
 }
