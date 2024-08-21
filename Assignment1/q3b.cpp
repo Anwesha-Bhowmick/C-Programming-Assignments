fa197fa85d3011e4bc70273304fcbcc2d54d31cf
Anwesha Bhowmick <113498742+Anwesha-Bhowmick@users.noreply.github.com>
2024-08-21 16:18:54 +0530

    Update q3b.cpp

--- Assignment1/q3b.cpp
+++ Assignment1/q3b.cpp
@@ -5,9 +5,9 @@ int main()
     float x, y;
     std::cout << "\nenter the value of y: ";
     std::cin >> y;
-    x = y - 20 * 6 / 9; //this happens because the division implicitly casts the result into integer and the final result is returned as an integer.
+    x = y - 20 * 6 / 9; /*this happens because the division implicitly casts the result into integer and the final result is returned as an integer.*/
     std::cout << "\nvalue of x before modification: " << x << "\n";
-    x = y - 20 * 6 / 9.0;//the value 9.0 changes the return type of the division operation to float and as a result the final value is returned as a float and not implicitly casted as an integer.
+    x = y - 20 * 6 / 9.0;/*the value 9.0 changes the return type of the division operation to float and as a result the final value is returned as a float and not implicitly casted as an integer.*/
     std::cout << "\nvalue of x after modification: " << x << "\n";
     return 0;
 }
