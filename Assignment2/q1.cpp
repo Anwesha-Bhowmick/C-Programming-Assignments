4723bf341f51c56d9ca17773b3783d2e50ce375a
Anwesha Bhowmick <113498742+Anwesha-Bhowmick@users.noreply.github.com>
2024-08-28 11:51:56 +0530

    Update q1.cpp

--- Assignment2/q1.cpp
+++ Assignment2/q1.cpp
@@ -1,18 +1,21 @@
-#include<iostream>
+#include <iostream>
+
+void function1(char *charPointer1, int &integerReference1); // declaring the function
+
+typedef void (*newAlias)(char *, int &); // alias
+
+newAlias funcPointer1; // declaring a function pointer to the "function1"
+
+void function2(newAlias); // declaring a function that takes such a pointer as a parameter
+
+newAlias function3(); // declaring a function returning such a pointer
+
+newAlias function4(newAlias) // defining a function which takes such pointer as parameter and return such pointer
+{
+    return funcPointer1;
+}
 
 int main()
 {
-  void function1(char *charPointer1, int &integerReference1);//declaring the function
-  //void (*funcPointer1)(char *, int &);//declaring a function pointer to the "function1" type of functions
-  //void function2(void (*funcPointer1)(char *, int &));//?
-  //void (*funcPointer1)(char *, int &) function3();//?
-  //void (*funcPointer1)(char *, int &) function4(void (*funcPointer1)(char *, int &));//?
-  
-  //OR
-  
-  typedef void (*newAlias)(char *, int &);//?
-  void function2(newAlias);//?
-  newAlias function3();//?
-  newAlias function4(newAlias);//?
-  return 0;
+    return 0;
 }
