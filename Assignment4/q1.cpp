4e337dcc4c51b8b51692b51c7a2502ef02f31092
Anwesha Bhowmick <113498742+Anwesha-Bhowmick@users.noreply.github.com>
2024-09-18 15:07:58 +0530

    Update q1.cpp

--- Assignment4/q1.cpp
+++ Assignment4/q1.cpp
@@ -6,7 +6,7 @@ char character1;
 char *pointer1 = &character1;                  // a pointer to a character
 int array1[10]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // an array of 10 integers
 int (&reference1)[10] = array1;                // a reference to an array of 10 integers
-char array2[5]{'a', 'b', 'c', 'd', 'e'};
+char array2[5]{'h', 'e', 'l', 'l', 'o'};
 char (*pointer2)[5] = &array2; // a pointer to an array of character strings
 char **pointer3 = &pointer1;
 const int integer1 = 10;                    // a constant integer
