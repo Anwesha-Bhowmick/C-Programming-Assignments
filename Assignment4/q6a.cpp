dd18adf84d9be8478c2ba045d4c506c2abc281bc
Anwesha Bhowmick <113498742+Anwesha-Bhowmick@users.noreply.github.com>
2024-09-18 13:14:02 +0530

    Update q6a.cpp

--- Assignment4/q6a.cpp
+++ Assignment4/q6a.cpp
@@ -14,6 +14,10 @@ int main()
         cout << "\nenter number of days in " << (i + 1) << "th month: ";
         cin >> monthDays[i];
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        /*ignore function ignores upto <1st parameter> number of characters in the input buffer or till it encounters a newline character, whichever comes first.
+        In order to get the maximum number of characters that can be on the input buffer, so as to skip those number of characters, we use numeric_limits<streamsize>::max */
+
+        /*This function is being used because from the second input onwards, the input for the name of the month is skipped, this happens because 31 followed by enter from the keyboard is basically '31\n', 31 goes into the variable but \n stays in the buffer and in the next iteration getline() receives it and stops the scan then again input for number of days is asked, essentially skipping the input for the name of the month*/
     }
 
     for (int i = 0; i < 12; i++)
