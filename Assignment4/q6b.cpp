0f443eaebdbe713e22127d0373ce6b2e8e4214f7
Anwesha Bhowmick <113498742+Anwesha-Bhowmick@users.noreply.github.com>
2024-09-18 14:24:22 +0530

    Add files via upload

---
+++ Assignment4/q6b.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#define MAX 50
+using namespace std;
+
+struct Month
+{
+    // const char *monthName;
+    const char *monthName;
+    int monthDays;
+};
+
+int main()
+{
+    Month year[12] = {
+        {"January", 31},
+        {"February", 28},
+        {"March", 31},
+        {"April", 30},
+        {"May", 31},
+        {"June", 30},
+        {"July", 31},
+        {"August", 31},
+        {"September", 30},
+        {"October", 31},
+        {"November", 30},
+        {"December", 31},
+    };
+
+    for (int i = 0; i < 12; i++)
+    {
+        cout << "     " << year[i].monthName << "     " << year[i].monthDays << endl;
+    }
+
+    return 0;
+}
